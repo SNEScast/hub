2c909b2dfc954ce26f78d2d34a8a2f759c48b43c
Florian Bottke <kontakt@seeseekey.net>
2024-04-19 08:43:45 +0200

    Build hub.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#sh
+
+# https://stackoverflow.com/questions/69665222/node-js-17-0-1-gatsby-error-digital-envelope-routinesunsupported-err-os
+export NODE_OPTIONS=--openssl-legacy-provider
+
+yarn build
+
+git add *
+git commit -m "Build hub."
+git push
